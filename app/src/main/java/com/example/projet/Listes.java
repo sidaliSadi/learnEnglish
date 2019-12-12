f18041e58f18e6cb4ff0c18237706dae3d40abac
AYAD BRAHAM <ayad@informatique.univ-paris-diderot.fr>
2019-12-12 18:38:55 +0100

    qlq Ta93ad

--- app/src/main/java/com/example/projet/Listes.java
+++ app/src/main/java/com/example/projet/Listes.java
@@ -1,9 +1,12 @@
 package com.example.projet;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -17,13 +20,15 @@ private ListView lview;
 private DataBaseManager db;
 private String [] listes;
 private TextView tv;
+private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listes);
         lview = findViewById(R.id.lView);
         tv = findViewById(R.id.tv);
-
+        toolbar = findViewById(R.id.toolbar1);
+        setSupportActionBar(toolbar);
         db = new DataBaseManager(this);
 
         List<String> l = db.readListes();
@@ -52,5 +57,27 @@ private TextView tv;
         Intent iii = new Intent(this, Traduction.class);
         iii.putExtra("nom_liste", s);
         startActivity(iii);
+    }
+
+
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_toolbar, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.quitter:
+                finish();
+                break;
+            case R.id.acceuil:
+                Intent i = new Intent(this,MainActivity.class);
+                startActivity(i);
+        }
+        return true;
     }
 }
